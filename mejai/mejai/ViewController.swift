a484008dbf507a545f46467bdc8af29835eed28a
noeyiz <lamoureclatant@gmail.com>
2024-10-25 04:02:32 +0900

    feat: 프로젝트 생성

---
+++ mejai/mejai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mejai
+//
+//  Created by 지연 on 10/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
